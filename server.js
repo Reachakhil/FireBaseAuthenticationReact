aef3ac985f24c6453c2cda54b1e49c8103523593
Reachakhil <41184471+Reachakhil@users.noreply.github.com>
2021-02-04 21:41:36 +0530

    changes

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const cors = require('cors');
 app.use(Router)
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
 
 
